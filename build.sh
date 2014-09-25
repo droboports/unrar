b2f6b9fdb5e8f41e80514345cbaaeb4e95bc0526
Ricardo Padilha <ricardospadilha@gmail.com>
2014-09-25 12:00:34 +0200

    First version

---
+++ build.sh
@@ -0,0 +1,109 @@
+#!/usr/bin/env bash
+
+### bash best practices ###
+# exit on error code
+set -o errexit
+# exit on unset variable
+set -o nounset
+# return error of last failed command in pipe
+set -o pipefail
+# expand aliases
+shopt -s expand_aliases
+# print trace
+set -o xtrace
+
+### logfile ###
+timestamp="$(date +%Y-%m-%d_%H-%M-%S)"
+logfile="logfile_${timestamp}.txt"
+echo "${0} ${@}" > "${logfile}"
+# save stdout to logfile
+exec 1> >(tee -a "${logfile}")
+# redirect errors to stdout
+exec 2> >(tee -a "${logfile}" >&2)
+
+### environment variables ###
+. crosscompile.sh
+export NAME="unrar"
+export DEST="/mnt/DroboFS/Shares/DroboApps/${NAME}"
+export DEPS="/mnt/DroboFS/Shares/DroboApps/${NAME}deps"
+export CFLAGS="$CFLAGS -Os -fPIC"
+export CXXFLAGS="$CXXFLAGS $CFLAGS"
+export CPPFLAGS="-I${DEPS}/include"
+export LDFLAGS="${LDFLAGS:-} -Wl,-rpath,${DEST}/lib -L${DEST}/lib"
+alias make="make -j8 V=1 VERBOSE=1"
+
+# $1: file
+# $2: url
+# $3: folder
+_download_tgz() {
+  [[ ! -f "download/${1}" ]] && wget -O "download/${1}" "${2}"
+  [[ -d "target/${3}" ]] && rm -v -fr "target/${3}"
+  [[ ! -d "target/${3}" ]] && tar -zxvf "download/${1}" -C target
+}
+
+# $1: folder
+# $2: url
+_download_git() {
+  [[ -d "target/${1}" ]] && rm -v -fr "target/${1}"
+  [[ ! -d "target/${1}" ]] && git clone "${2}" "target/${1}"
+}
+
+### UNRAR ###
+_build_unrar() {
+local VERSION="5.1.7"
+local FOLDER="unrar"
+local FILE="unrarsrc-${VERSION}.tar.gz"
+local URL="http://www.rarlab.com/rar/${FILE}"
+
+_download_tgz "${FILE}" "${URL}" "${FOLDER}"
+pushd target/"${FOLDER}"
+mv makefile Makefile
+make CXX="${CXX}" CXXFLAGS="${CFLAGS} -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE" STRIP="${STRIP}" LDFLAGS="${LDFLAGS} -pthread"
+make install DESTDIR="${DEST}"
+popd
+}
+
+### BUILD ###
+_build() {
+  _build_unrar
+  _package
+}
+
+_create_tgz() {
+  local appname="$(basename ${PWD})"
+  local appfile="${PWD}/${appname}.tgz"
+
+  if [[ -f "${appfile}" ]]; then
+    rm -v "${appfile}"
+  fi
+
+  pushd "${DEST}"
+  tar --verbose --create --numeric-owner --owner=0 --group=0 --gzip --file "${appfile}" *
+  popd
+}
+
+_package() {
+  cp -v -aR src/dest/* "${DEST}"/
+  find "${DEST}" -name "._*" -print -delete
+  _create_tgz
+}
+
+_clean() {
+  rm -v -fr "${DEPS}"
+  rm -v -fr "${DEST}"
+  rm -v -fr target/*
+}
+
+_dist_clean() {
+  _clean
+  rm -v -f logfile*
+  rm -v -fr download/*
+}
+
+case "${1:-}" in
+  clean)     _clean ;;
+  distclean) _dist_clean ;;
+  package)   _package ;;
+  "")        _build ;;
+  *)         _build_${1} ;;
+esac
